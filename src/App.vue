904098803dc459e02ff65fa1db14862e12752548
atolbat <31169768+atolbat@users.noreply.github.com>
2023-04-24 21:46:44 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ItemsPanel from './components/ItemsPanel.vue';
+</script>
+
+<template>
+  <ItemsPanel></ItemsPanel>
+</template>
+
+<style scoped></style>
---
+++ src/components/ItemsPanel.vue
@@ -0,0 +1,64 @@
+<template>
+  <aside class="w-10 h-screen flex">
+    <ul>
+      <li
+        v-for="(val, i) in items"
+        :key="val"
+        class="
+          w-10
+          h-10
+          rounded-md
+          bg-slate-200
+          my-1
+          cursor-pointer
+          hover:bg-slate-300
+        "
+        :style="{
+          transitionDelay: `${i}ms`,
+        }"
+      >
+        {{ i }}
+      </li>
+    </ul>
+
+    <ul class="ml-1">
+      <li
+        v-for="(val, i) in items"
+        :key="val"
+        class="
+          w-10
+          h-10
+          rounded-md
+          bg-slate-200
+          my-1
+          cursor-pointer
+          hover:bg-slate-300
+        "
+        :style="{
+          transitionDelay: `${i}ms`,
+        }"
+      >
+        {{ i }}
+      </li>
+    </ul>
+  </aside>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+const items = ref([{}, {}, {}, {}, {}, {}, {}, {}]);
+</script>
+
+<style>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.list-leave-active {
+  position: absolute;
+}
+</style>
